22bd851538ea55740beae205b1418d7bb0834820
kgmccann <31707840+kgmccann@users.noreply.github.com>
2019-04-17 12:10:36 -0400

    Added vars sql.orderby, asc, and additional logic
    Adds "ORDER BY" grammar to cosmos query

--- R/constructQuery.R
+++ R/constructQuery.R
@@ -2,19 +2,27 @@
 #' 
 #' @param sql.what String for specifying what fields to retrieve. Typically called select condition
 #' @param sql.where String for specifying what filter to use on data. Typically called search condition
+#' @param sql.orderby String for specifying what field to order query by.
+#' @param asc Logical for Is the order ascending.
 #' @return String containing a full SELECT statement for the database
 #' @keywords query select where
 #' @export
 #' @examples
 #' constructQuery(sql.what = "c.contact.eloquaId", sql.where = "c.contact.eloquaId != null")
 
-constructQuery <- function(sql.what, sql.where) {
-
+constructQuery <- function(sql.what, sql.where, sql.orderby, asc) {
+    if(!asc){
+        sql.rank <- 'DESC'
+    } else{
+        sql.rank <- 'ASC'
+    }
     # Create the query using predicate if it exists
-    if (sql.where == "") {
+    if (sql.where == "" & sql.orderby == "") {
         full.query <- paste("SELECT", sql.what, "FROM c", sep = " ")
-    } else {
+    } else if(sql.orderby == ""){
         full.query <- paste("SELECT ", sql.what, " FROM c WHERE (", sql.where, ")", sep = "")
+    } else{
+        full.query <- paste("SELECT ", sql.what, " FROM c WHERE (", sql.where, ")"," ORDER BY (",sql.orderby , ")", sql.rank,sep = "")
     }
 
     # Return the query
